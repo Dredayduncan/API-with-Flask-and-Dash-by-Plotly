de5b49f1372fcaf076792b162ede1b007f24ecff
Dredayduncan <dredayduncan@gmail.com>
2020-12-29 15:53:58 +0000

    Reorganised files to fit repository requirements

--- Tracking Conference Attendance/atendee.py
+++
@@ -1,8 +0,0 @@
-class Attendee:
-
-    def __init__(self, name, company, state, email):
-        self.name = name
-        self.company = company
-        self.state = state
-        self.email = email
-
